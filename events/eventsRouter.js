014774049726d43e4d375541e36cd1ee3e20dcd9
eric <eric355402@yahoo.com>
2020-06-16 20:43:27 -0500

    set up knex, added database folder with dbconfig, added dotenv

---
+++ events/eventsRouter.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Events = require("./eventsModel");
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  if (req.body.challenge) {
+    res.status(200);
+    res.contentType("text/plain");
+    console.log(req.body);
+    res.send(req.body.challenge);
+  } else {
+    Events.addEvent(req.body.event);
+  }
+});
+
+router.get("/event", (req, res) => {
+  Events.getEvent().then((event) => {
+    console.log(event);
+    res.status(200);
+    res.send(event);
+  });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const slackRouter = require('./slack/api')
+const eventRouter = require("./events/eventsRouter");
 
-app.use('/api', slackRouter);
+app.use("/event", eventRouter);
 app.use(cors());
 app.use(helmet());
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
