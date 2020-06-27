4651eae4ccdf1fb7f474505c532d9df4d7bc6c94
Kyle Pace <46023695+kpace622@users.noreply.github.com>
2020-06-26 20:25:39 -0500

    fixing knex migration mistake (#30)

---
+++ migrations/20200617183432_Slack_User.js
@@ -0,0 +1,12 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('slack_user', tbl => {
+    tbl.increments();
+
+    tbl.string('slack_user', 255).notNullable().unique();
+    tbl.integer('user_id').references("users.id").unsigned().onDelete("CASCADE");
+  });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('slack_user')
+};
