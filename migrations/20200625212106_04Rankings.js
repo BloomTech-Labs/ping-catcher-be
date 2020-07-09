25ae8840229ad1a4210798f5c01abd8d8050ab32
Kyle Pace <kpace2296@gmail.com>
2020-07-08 21:56:14 -0500

    added meta event table

--- migrations/20200625212106_04Rankings.js
+++ migrations/20200625212106_04Rankings.js
@@ -9,7 +9,7 @@ exports.up = function(knex) {
         tbl.increments();
         tbl.integer('rankings_id').references('rankings.id').notNullable().unsigned().onDelete('CASCADE')
         tbl.integer('slack_user').references('slack_user.id').unsigned().onDelete('CASCADE')
-        tbl.integer('event_id').references('events.id').unsigned().onDelete('CASCADE')
+        tbl.integer('event_id').references('meta_event.id').unsigned().onDelete("CASCADE")
         tbl.string('last_accessed', 255)
         tbl.string('nickname', 255).notNullable()
   })
