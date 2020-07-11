c4b4bfa3e2aeb26161905db431666393f941874c
Kyle Pace <46023695+kpace622@users.noreply.github.com>
2020-07-10 22:05:01 -0500

    changing to environment variable (#48)

--- database/db-config.js
+++ database/db-config.js
@@ -2,6 +2,6 @@ const knex = require("knex");
 
 const config = require("../knexfile.js");
 
-const db = knex(config[NODE_ENV || "production"]);
+const db = knex(config[process.env.NODE_ENV || "production"]);
 
 module.exports = db;
