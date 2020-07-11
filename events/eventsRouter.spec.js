28519b8430288d97b8f992ebe73ba595645e5596
Kyle Pace <kpace2296@gmail.com>
2020-07-10 21:36:37 -0500

    adding test database

---
+++ events/eventsRouter.spec.js
@@ -0,0 +1,8 @@
+const db = require('../database/db-config');
+const Events = require('./eventsModel');
+
+describe('events model', () => {
+  describe('insert', () => {
+    it('should insert event into the db')
+  })
+})
