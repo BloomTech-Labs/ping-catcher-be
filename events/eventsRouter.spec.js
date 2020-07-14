b4ae9224acb9f7b83c07fd253d3da2933a39d96d
Kyle Pace <kpace2296@gmail.com>
2020-07-13 20:51:38 -0500

    testing the testing

--- events/eventsRouter.spec.js
+++ events/eventsRouter.spec.js
@@ -3,6 +3,16 @@ const Events = require('./eventsModel');
 
 describe('events model', () => {
   describe('insert', () => {
-    it('should insert event into the db')
+    it('should insert events into the db', async () => {
+      await Events.add({text: "testing"});
+
+      const addedEvents = await db('events')
+      expect(addedEvents).toHaveLength(2);
+    });
+
+    it('should insert event into the db', async () => {
+      let event = await Events.add({text: "Testing the testing database"})
+      expect(event.text).toBe('Testing the testing database')
+    })
   })
 })
--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
 
   testing: {
     client: "pg",
-    connection: process.env.HEROKU_POSTGRESQL_AMBER_URL,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
