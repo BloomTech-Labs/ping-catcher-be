c048f7bd1ff5e6480d4274641cad58e362238383
Kyle Pace <kpace2296@gmail.com>
2020-07-28 23:21:33 -0500

    moved users add function to the catch statement

--- api/events/eventsModel.js
+++ api/events/eventsModel.js
@@ -35,5 +35,5 @@ function add(event) {
   // console.log(slack_username);
   // console.log(slack_user_id);
   const newEvent = { type, text, slack_user, team, channel, timestamp, event_timestamp};
-  return db("events").insert(newEvent).returning('id');
+  return db("events").insert(newEvent).returning('id'); //change to async function?
 }
