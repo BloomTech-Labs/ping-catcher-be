acfb72b1fe5a4aebe5413982311bdf1fc28058b4
Kyle Pace <kpace2296@gmail.com>
2020-08-03 20:44:04 -0500

    added logic for adding subscriptions

--- api/metaEvents/metaEventsModel.js
+++ api/metaEvents/metaEventsModel.js
@@ -1,10 +1,15 @@
 const db = require('../../database/db-config');
 
 module.exports = {
-  findByText
+  findByText,
+  add
 }
 
 function findByText(stringObject) {
   console.log("Inside of meta events find by text", stringObject)
   return db('meta_events').where(stringObject).returning('id')
+}
+
+function add({stringObject}) {
+  return db('meta_events').where({stringObject}).returning('id');
 }
--- api/rankings/threadRankingRouter.js
+++ api/rankings/threadRankingRouter.js
@@ -17,6 +17,4 @@ router.get('/:text', (req, res) => {
     })
 })
 
-
-
 module.exports = router;
