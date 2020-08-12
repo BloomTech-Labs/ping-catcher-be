d8a3ea954552a90f85a5a06a2addba3a5a5574a3
Kyle Pace <kpace2296@gmail.com>
2020-08-11 23:00:50 -0500

    fixed typo in alter table

--- migrations/20200811223649_06slackUser.js
+++ migrations/20200811223649_06slackUser.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema.alterTable('slack_user', tbl => {
-    tbl.integer('ranking_id').references("rankings.id").unsigned().onoDelete("CASCADE")
+    tbl.integer('ranking_id').references("rankings.id").unsigned().onDelete("CASCADE")
   });
 };
 
