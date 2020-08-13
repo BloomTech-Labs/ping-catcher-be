43dd72c406499b96c63bf01d93641b62a776cbf5
Kyle Pace <46023695+kpace622@users.noreply.github.com>
2020-08-13 13:35:59 -0500

    moving table drops to fix bug (#110)

--- migrations/20200710225507_05Rankings.js
+++ migrations/20200710225507_05Rankings.js
@@ -16,7 +16,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-    return knex.schema
-        .dropTableIfExists('thread_ranking')
-        .dropTableIfExists('rankings')
+
 };
