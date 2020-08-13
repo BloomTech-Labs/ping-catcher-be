7c05f9286d71a3a93b1da43034c2b3bb10448db7
Kyle Pace <kpace2296@gmail.com>
2020-08-13 10:29:49 -0500

    Working on migrations

--- migrations/20200811223649_06slackUser.js
+++ migrations/20200811223649_06slackUser.js
@@ -1,10 +1,10 @@
 
 exports.up = function(knex) {
   return knex.schema.alterTable('slack_user', tbl => {
-    tbl.integer('ranking_id').unsigned()
+    tbl.integer('ranking_id').references("rankings.id").unsigned().onDelete("CASCADE")
   });
 };
 
 exports.down = function(knex) {
-
+  return knex.schema.dropTableIfExists('slack_user')
 };
