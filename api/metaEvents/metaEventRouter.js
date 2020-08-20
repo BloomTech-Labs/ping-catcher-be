62a6dd98a4352d67ad60c05aed945c67e023b0b6
Kyle Pace <46023695+kpace622@users.noreply.github.com>
2020-08-20 00:00:55 -0500

    adding meta event using a try catch block (#138)

--- api/metaEvents/metaEventRouter.js
+++ api/metaEvents/metaEventRouter.js
@@ -81,25 +81,60 @@ router.post("/newSubscription", (req, res) => {
   };
   res.set(headers);
 
-  SlackUser.findByName({ slack_username: slackUser })
-    .then((userResponse) => {
-      // Search for existing slack user, if not found code doesn't run
-      console.log(userResponse); // Looks for an existing ranking, if not found, will jump to catch statement to add a ranking for the slack user
-      const rankResponse = Ranking.findById({id: userResponse.ranking_id})
-      return [rankResponse, userResponse]
-      // return [ Ranking.findById({ id: userResponse.ranking_id }), userResponse ];
-    })
-    .then((rankResponseArr) => {
-      const [rankResponse, userResponse] = rankResponseArr;
-      console.log("rank response", rankResponse)
-      if (rankResponse === -1) {
-        const ranking_id = Ranking.add({ user_id: userResponse.user_id }); // Start here.
-        SlackUser.update({ id: slackUser.id, update: {ranking_id} });
-        addMetaEvent({ res, slackUser: slackUser.slack_username, rankResponse: ranking_id, event_key });
-      } else {
-        addMetaEvent({ res, rankResponse, slackUser: slackUser.slack_username, event_key });
+  async function findRanking() {
+    let userResponse
+    let rankResponse
+    let ranking_id
+    try{
+      userResponse = await SlackUser.findByName({ slack_username: slackUser })
+    }
+    catch(err){
+      console.log(err)
+    }
+    try{
+      rankResponse = await Ranking.findById({ id: userResponse.ranking_id })
+    }
+    catch(err){
+      console.log(err)
+    }
+    if(rankResponse === -1) {
+      try{
+        ranking_id = await Ranking.add({ user_id: userResponse.user_id })
       }
-    });
+      catch(err){
+        console.log(err)
+      }
+      try{
+        await SlackUser.update({ id: slackUser.id, update: {ranking_id} })
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+    addMetaEvent({ res, slackUser: slackUser.slack_username, rankResponse: ranking_id, event_key })
+  }
+
+  findRanking();
+
+//   SlackUser.findByName({ slack_username: slackUser })
+//     .then((userResponse) => {
+//       // Search for existing slack user, if not found code doesn't run
+//       console.log(userResponse); // Looks for an existing ranking, if not found, will jump to catch statement to add a ranking for the slack user
+//       const rankResponse = Ranking.findById({id: userResponse.ranking_id})
+//       return [rankResponse, userResponse]
+//       // return [ Ranking.findById({ id: userResponse.ranking_id }), userResponse ];
+//     })
+//     .then((rankResponseArr) => {
+//       const [rankResponse, userResponse] = rankResponseArr;
+//       console.log("rank response", rankResponse)
+//       if (rankResponse === -1) {
+//         const ranking_id = Ranking.add({ user_id: userResponse.user_id }); // Start here.
+//         SlackUser.update({ id: slackUser.id, update: {ranking_id} });
+//         addMetaEvent({ res, slackUser: slackUser.slack_username, rankResponse: ranking_id, event_key });
+//       } else {
+//         addMetaEvent({ res, rankResponse, slackUser: slackUser.slack_username, event_key });
+//       }
+//     });
 });
 
 module.exports = router;
