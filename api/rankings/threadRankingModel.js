1653b81c15f74f2d35734567997a926a59f2ae22
Kyle Pace <kpace2296@gmail.com>
2020-08-20 00:35:42 -0500

    switched db table

--- api/rankings/threadRankingModel.js
+++ api/rankings/threadRankingModel.js
@@ -10,8 +10,8 @@ function find(rankings_id) {
   return db('thread_ranking').where({rankings_id})
 }
 
-function add({event_id, nickname, rankings_id, slack_user}) {
-  return db('rankings').insert({event_id, nickname, rankings_id, slack_user})
+function add({event_id, nickname, rankings_id, slack_user, last_accessed}) {
+  return db('thread_ranking').insert({event_id, nickname, rankings_id, slack_user, last_accessed})
 }
 
 function findByText(text) {
